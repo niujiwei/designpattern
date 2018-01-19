28728bf07c3acd22d1ac7f0649d1406fb41766b0
niujiwei <747271409@qq.com>
2018-01-19 16:27:48 +0800

    初始化

---
+++ designPattern/src/main/java/com/niujiwei/group/App.java
@@ -0,0 +1,11 @@
+package com.niujiwei.group;
+
+/**
+ * Hello world!
+ * @author niujiwei
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!-develop");
+    }
+}
